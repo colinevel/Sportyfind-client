bd5a84edfa1d0e1271458884ccd02ea50902e0f2
MehmetCan75 <mehmetozmenfb@gmail.com>
2020-02-27 11:49:52 +0100

    createUser

--- sportyfind-client/src/App.js
+++ sportyfind-client/src/App.js
@@ -1,25 +1,15 @@
 import React from 'react';
+import { Route, Switch } from "react-router-dom";
 import logo from './logo.svg';
-import './App.css';
+import './styles/App.css';
+import CreateEvent from './components/form/CreateEvent'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+Hello Sportyfind members
     </div>
+  
   );
 }
 
