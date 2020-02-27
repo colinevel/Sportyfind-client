612331b6f0ad4945e39067a6eaea871351898c64
Baptiste WARGNY <bwargny92@hotmail.com>
2020-02-27 11:42:42 +0100

    all views

--- sportyfind-client/src/App.js
+++ sportyfind-client/src/App.js
@@ -1,26 +1,73 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { Route, Switch } from "react-router-dom";
+// pages components
+import CreateEvent from "./views/CreateEvent";
+import Events from "./views/Events";
+import Event from "./views/Event";
+import MyDashboard from "./views/MyDashboard";
+import Home from "./views/Home";
+import Signin from "./views/Signin";
+import Signup from "./views/Signup";
+import UpdateEvent from "./views/UpdateEvent";
+import UpdateUser from "./views/UpdateUser";
+import User from "./views/User";
+import NotFound from "./views/NotFound";
 
-function App() {
+
+// auth
+import { useAuth } from "./auth/useAuth";
+import UserContext from "./auth/UserContext";
+import { ProtectedRoute } from "./auth/ProtectedRoute";
+
+
+
+
+export default function App() {
+  const { isLoading } = useAuth();
+  
+  const [currentUser, setCurrentUser] = useState({});
+
+  // check src/auth/UserContext =>
+  // MANDATORY TO GET/SET loggedin currentUser against server response
+  const UserContextValue = {
+    currentUser,
+    setCurrentUser
+  };
+
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // the context provider will make currentUser informations down the component tree
+    // check src/auth/UserContext
+    <UserContext.Provider value={UserContextValue}>
+      {isLoading ? (
+        null
+      ) : (
+        <React.Fragment>
+         
+          <main id="content_main">
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route exact path="/events" component={Events} />
+              <Route path="/events/:id" component={Event} />
+              <Route path="/signin" component={Signin} />
+              <Route path="/signup" component={Signup} />
+              {/* check the protected route in src/auth folder */}
+              <ProtectedRoute path="/users/dashboard" component={MyDashboard} />
+              <ProtectedRoute path="/users/:id" component={User} />
+              <ProtectedRoute path="/users/edit/:id" component={UpdateUser} />
+              <ProtectedRoute path="/events/create" component={CreateEvent} />
+              <ProtectedRoute path="/events/edit/:id" component={UpdateEvent} />
+           
+              
+              <Route path="*" component={NotFound} />
+            </Switch>
+          </main>
+        </React.Fragment>
+      )}
+    </UserContext.Provider>
   );
-}
+};
 
-export default App;
+
+
