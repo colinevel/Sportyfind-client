e444edb9c4927fbd453db1fd230443a2881505ea
MehmetCan75 <mehmetozmenfb@gmail.com>
2020-02-28 11:07:38 +0100

    create and edit

--- sportyfind-client/src/views/Event.jsx
+++ sportyfind-client/src/views/Event.jsx
@@ -1,11 +1,53 @@
-import React, { Component } from 'react'
+import React, { useContext, useState, useEffect } from "react";
+// custom tools
+import apiHandler from "../api/APIHandler";
+// import CardAlbum from "../components/card/CardAlbum";
+// import Comments from "../components/comment/Comments";
+// import List from "../components/List";
+// import Stars from "../components/star/Stars";
+import UserContext from "./../auth/UserContext";
 
-export default class Event extends Component {
-    render() {
-        return (
-            <div>
-                EVENT DETAILS
-            </div>
-        )
+// styles
+// Don't forget to add the css file
+
+React.createContext({
+  currentUser: null,
+  setCurrentUser: () => {}
+});
+
+export default function Event({ match }) {
+
+  const userContext = useContext(UserContext);
+  const { currentUser } = userContext;
+
+  const [event, setEvent] = useState(null)
+
+  console.log(event)
+
+  useEffect(() => {
+
+    const getData = async () => {
+      const eventRes = await apiHandler.get(`/events/${match.params.id}`);
+      setEvent(eventRes.data);
     }
+
+    getData()
+
+  }, []);
+
+  return (
+    <div className="">
+        <div className="">{event && event.name}</div>
+        <div className="">{event && event.sport}</div>
+        <div className="">{event && event.name}</div>
+        <div className="">{event && event.name}</div>
+        <div className="">{event && event.name}</div>
+        <div className="">{event && event.name}</div>
+        <div className="">{event && event.name}</div>
+        <div className="">{event && event.name}</div>
+        
+     
+      </div>
+
+  );
 }
