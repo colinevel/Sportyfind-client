4ebbe1913cfce4664fba4f1726f7192349ae6675
Baptiste WARGNY <bwargny92@hotmail.com>
2020-03-03 10:30:50 +0100

    merging problems

--- sportyfind-client/src/views/Event.jsx
+++ sportyfind-client/src/views/Event.jsx
@@ -9,6 +9,7 @@ import UserContext from "./../auth/UserContext";
 import MapsContainer from "./../components/MapsContainer"
 import Buttons from "./../components/Buttons"
 import { Link } from "react-router-dom";
+import moment from 'moment';
 
 // styles
 import "../styles/event.css"
@@ -44,58 +45,39 @@ export default function Event({ match, history }) {
 
   }, []);
 
+
+
   return (
     <div>
       <div><h1 className="eventtitle">Event Details</h1></div>
-
       <div className="eventdetails">
         <div className="eventdescr">
-<<<<<<< HEAD
           <div className="details">Name of the event: {event && event.name}</div>
           <div className="details">Sport: {event && event.sport.name} {event && event.sport.logo}</div>
-          <div className="details">Event Date&Hour: {event && event.date}</div>
-          <div className="details">Event's creator: {event && event.creator}</div>
-          <div className="details">Participants: {event && event.participants}</div>
+          <div className="details">Event Date: {event && moment(event.date).format("MMMM Do YYYY")}</div>
+          <div className="details">Event Time: {event && event.time}</div>
+          <div className="details">Event's creator: {event && event.creator.username}</div>
+          <div className="details">Participants: <ul>{event && event.participants.map((p, i) => <li key={i}>{p.username}</li>)} </ul></div>
           <div className="details">Max participants: {event && event.maxParticipants}</div>
         </div>
         <div className="details">Event's localisation: {event && event.localisation}
           <div className="googlemap">
-            {event && <MapsContainer lat={event.lat} lng={event.lng} />}
-=======
-        <div className="details">Name of the event: {event && event.name}</div>
-        <div className="details">Sport: {event && event.sport.name} {event && event.sport.logo}</div>
-        <div className="details">Event Date: {event && event.date}</div>
-        <div className="details">Event Time: {event && event.time}</div>
-        <div className="details">Event's creator: {event && event.creator}</div>
-        <div className="details">Participants: {event && event.participants}</div>
-        <div className="details">Max participants: {event && event.maxParticipants}</div> 
-        </div>
-        <div className="details">Event's localisation: {event && event.localisation}
-          <div className="googlemap">
-          {event &&  <MapsContainer style={{width: "50%", height: "50%"}} lat={event.lat} lng={event.lng}/>}
->>>>>>> f72c2be2a6bb378e1b00d47f4bd0ef344182f833
+            {event && <MapsContainer style={{ width: "50%", height: "50%" }} lat={event.lat} lng={event.lng} />}
           </div>
         </div>
       </div>
 
+
+
       <div className="adminbuttons">
-<<<<<<< HEAD
         <Buttons match={match} event={event} history={history} />
 
-=======
-    <button className="btndeleteevent" onClick={deleteEvent}> Delete </button>
-    <button className="btnjoinevent"> Join </button>
-    <button className="btnleaveevent"> Leave </button>
-    <Link to={`/events/edit/${match.params.id}`}><button className="btneditevent"> Edit </button>
-    </Link>
-    </div>
->>>>>>> f72c2be2a6bb378e1b00d47f4bd0ef344182f833
 
         {event ? currentUser._id === event.creator && (
           <Link to={`/events/edit/${match.params.id}`}> <button className="btneditevent"> Edit </button>
           </Link>
         ) : <p>NO DATA YET</p>}
-        
+
       </div>
 
     </div>
