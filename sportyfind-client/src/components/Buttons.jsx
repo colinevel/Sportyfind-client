510187ce6bec1bf38d5dc18efae4fda199f5a816
MehmetCan75 <mehmetozmenfb@gmail.com>
2020-03-03 17:49:17 +0100

    mehmet work

--- sportyfind-client/src/components/Buttons.jsx
+++ sportyfind-client/src/components/Buttons.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState, Component } from "react";
 import { useAuth } from "../auth/useAuth";
 import apiHandler from "../api/APIHandler";
 
@@ -6,10 +6,13 @@ import apiHandler from "../api/APIHandler";
 
 export default function Buttons({ match, event, history }) {
     const { isLoading, currentUser } = useAuth();
+    console.log(history, "match")
 
     if (isLoading) return null;
 
-
+    // const joinEvent = async () => {
+    //     const eventRes = await apiHandler.patch("/events", match.params.id);
+    // }
 
     const deleteEvent = async () => {
         const eventRes = await apiHandler.delete("/events", match.params.id);
@@ -20,6 +23,8 @@ export default function Buttons({ match, event, history }) {
     }
 
 
+
+
     return (
         <div className="buttons">
 
@@ -27,11 +32,13 @@ export default function Buttons({ match, event, history }) {
                 <button className="btndeleteevent" onClick={deleteEvent}> Delete </button>
             ) : <p>NO DATA YET</p>}
 
-            {event ? event.participants.includes(currentUser._id) && currentUser._id !== event.creator && (<button className="btnleaveevent"> Leave </button>
+            {event ? event.participants.includes(currentUser._id) && currentUser._id !== event.creator && (
+                <button className="btnleaveevent" > Leave </button>
             ) : <p>NO DATA YET</p>}
 
-            {event ? !event.participants.includes(currentUser._id) && (<button className="btnjoinevent"> Join </button>
-            ) : <p>NO DATA YET</p>}
+            {event && event.participants.includes(currentUser._id) ? <p>NO DATA YET</p> :
+            (<button className="btnjoinevent"> Join </button>)
+            }
 
         </div>
     )
