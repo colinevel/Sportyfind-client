f2f861ec0ec2f16b3bdc9a058db1aefd01769903
colinevel <coline.velard@gmail.com>
2020-03-05 18:34:54 +0100

    big fix

--- sportyfind-client/src/components/navbar/Logo.jsx
+++ sportyfind-client/src/components/navbar/Logo.jsx
@@ -6,7 +6,7 @@ export default function Logo() {
     return (
         <Link exact to="/" className="logoContainer">
         <div className="logo">
-            <img id="logo" src="../../../images/sportyfind_logo-removebg-preview.png" alt="logo" />
+            <img id="logo" src="/images/logo-sportyfind.png" alt="logo" />
         </div>
         </Link>
     )
