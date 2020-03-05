4d6536f379cde4190f1eaa68eb5becb063dc2b71
MehmetCan75 <mehmetozmenfb@gmail.com>
2020-03-05 12:40:36 +0100

    css

---
+++ sportyfind-client/src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React, { useEffect, useState } from "react";
+import Logo from "./../components/navbar/Logo";
+import NavLinks from "./../components/navbar/NavLinks";
+import "./../styles/header.css";
+import IconAvatarDisplay from "../components/icon/IconAvatarDisplay";
+
+
+export default function Header () {
+  return (
+
+    <div>    
+
+    </div>
+  )
+}
